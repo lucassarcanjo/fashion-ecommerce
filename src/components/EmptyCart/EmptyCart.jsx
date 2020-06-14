91c2d2bdc9d828ab38fdcb30bc6e38b95b324a2c
lucassarcanjo <lucassarcanjo@gmail.com>
2020-06-14 00:54:49 -0300

    Inclusao de tela amigavel para carrinho vazio

---
+++ src/components/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import emptyCartImg from "../../assets/img/empty-cart.jpeg";
+import "./EmptyCart.scss";
+
+export const EmptyCart = () => {
+  return (
+    <section className="empty-cart">
+      <img src={emptyCartImg} alt="Carrinho Vazio" />
+      <h2>Seu carrinho está vazio</h2>
+    </section>
+  );
+};
