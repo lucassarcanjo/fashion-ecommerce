21423360ec1e04e6777438d95baa1ce6ee376574
lucassarcanjo <lucassarcanjo@gmail.com>
2020-06-14 14:55:13 -0300

    Correcoes no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,13 +10,13 @@ const Footer = () => {
         <span role="img" aria-label="love">
           ❤️
         </span>
-        &nbsp;by&nbsp;
+        &nbsp;by
         <a
           href="https://github.com/lucassarcanjo"
           target="_blank"
           rel="noreferrer noopener"
         >
-          &nbsp;Lucas Arcanjo
+          Lucas Arcanjo
         </a>
       </div>
     </footer>
