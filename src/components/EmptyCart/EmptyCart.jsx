24f78806eb896fdaf2bc3daf613154740f8a6ea0
Lucas Arcanjo <lucassarcanjo@gmail.com>
2020-06-15 01:07:00 -0300

    Melhorias na pagina de carrinho vazio

--- src/components/EmptyCart/EmptyCart.jsx
+++ src/components/EmptyCart/EmptyCart.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import emptyCartImg from "../../assets/img/empty-cart.jpeg";
+import emptyCartImg from "../../assets/img/empty-cart.svg";
 import "./EmptyCart.scss";
 
 const EmptyCart = () => {
   return (
     <section className="empty-cart">
       <img src={emptyCartImg} alt="Carrinho Vazio" />
-      <h2>Seu carrinho está vazio</h2>
+      <h2>Seu carrinho está vazio <span role="img">😕</span></h2>
     </section>
   );
 };
